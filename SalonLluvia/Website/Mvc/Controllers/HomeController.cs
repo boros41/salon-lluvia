8f7a1770401293b7db434c6a6a00a783f3fb218c
boros41 <kevstar421@gmail.com>
2026-01-21 22:07:04 -0600

    Install EF Core & Refactor Breadcrumb
    - Removed code duplication for breadcrumb from HomeController

--- SalonLluvia/Website/Mvc/Controllers/HomeController.cs
+++ SalonLluvia/Website/Mvc/Controllers/HomeController.cs
@@ -13,9 +13,6 @@ namespace Mvc.Controllers
 
         public IActionResult About()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "About Us";
-            ViewBag.Action = "About"; 
             return View();
         }
 
@@ -26,44 +23,26 @@ namespace Mvc.Controllers
 
         public IActionResult Appointment()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "Appointment";
-            ViewBag.Action = "Appointment";
             return View();
         }
         public IActionResult Service()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "Services";
-            ViewBag.Action = "Service";
             return View();
         }
         public IActionResult Pricing()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "Prices";
-            ViewBag.Action = "Pricing";
             return View();
         }
         public IActionResult Gallery()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "Gallery";
-            ViewBag.Action = "Gallery";
             return View();
         }
         public IActionResult Team()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "Meet the Team";
-            ViewBag.Action = "Team";
             return View();
         }
         public IActionResult Testimonial()
         {
-            // used in _breadcrumbPartial
-            ViewBag.Header = "Testimonial";
-            ViewBag.Action = "Testimonial";
             return View();
         }
 
