067e09ef9482b466dc053816d9e8f48406c057e8
Boros <kevstar421@gmail.com>
2026-02-24 21:44:57 -0500

    Update to Bootstrap v5.3.8

--- SalonLluvia/Website/Mvc/wwwroot/js/navbar.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/*const $navItems = $(".nav-item");
-
-$(".nav-item").on("click", function () {
-    $(this).toggleClass("active");
-    console.log("Toggling: " + this);
-});*/ 
-//# sourceMappingURL=navbar.js.map
